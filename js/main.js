d4886fb436adf90f5b0b241f460bc0ab98988c72
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-07 23:49:11 +0530

    changing style properties

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ const view2 = document.querySelector("#view2");
 console.log(view2);
 
 view1.style.display = "flex";
-view2.style.display = "flex";
+view2.style.display = "none";
 
-/*
+
 const views = document.getElementsByClassName("view");
 console.log(views);
 
@@ -20,14 +20,15 @@ console.log(divs);
 
 const sameDivs = document.querySelectorAll("div");
 console.log(sameDivs);
-*/
+
 const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
 console.log(evenDivs);
 
-
-
-
-
+for( i = 0; i < evenDivs.length; i++) {
+    evenDivs[i].style.backgroundColor = "orange";
+    evenDivs[i].style.width = "200px";
+    evenDivs[i].style.height = "200px";
+}
 
 
 
