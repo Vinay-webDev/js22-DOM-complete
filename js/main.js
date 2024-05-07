bb87b2a9bdc333eba5706cf8fa0855ebe64b2c54
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-07 13:44:38 +0530

    getElementById and querySelector

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Document object model
+const view1 = document.getElementById("view1");
+console.log(view1);
+
+const view2 = document.querySelector("#view2");
+console.log(view2);
+
+view1.style.display = "flex";
+view2.style.display = "flex";
+
+/*
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+const sameViews = document.querySelectorAll(".view");
+console.log(sameViews);
+
+const divs = document.getElementsByTagName("div");
+console.log(divs);
+
+const sameDivs = document.querySelectorAll("div");
+console.log(sameDivs);
+*/
+const evenDivs = view1.querySelectorAll("div: nth-of-type(2n)");
+console.log(evenDivs);
+
+
+
+
+
+
+
+
+
+
+
